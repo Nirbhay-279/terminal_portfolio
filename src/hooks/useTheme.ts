5aa7c5580d3eb6f894a7b8acde7f9f30df782dec
Nirbhay <143536386+Nirbhay-279@users.noreply.github.com>
2025-07-01 04:41:04 +0530

    Update useTheme.ts

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import themes from "../components/styles/themes";
 import { setToLS, getFromLS } from "../utils/storage";
-import { DefaultTheme } from "styled-components";
+import type { DefaultTheme } from "styled-components";
 
 export const useTheme = () => {
   const [theme, setTheme] = useState<DefaultTheme>(themes.ubuntu);
